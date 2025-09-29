91f05f903d603b8de11d16a34f9ad65b47a2ab63
Ubuntu <ubuntu@ip-172-31-29-108.eu-central-1.compute.internal>
2025-09-29 15:30:08 +0000

    fix

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const { log } = require('console');
 
 // Create Express app
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3001;
 
 // Middleware
 app.use(express.json());
